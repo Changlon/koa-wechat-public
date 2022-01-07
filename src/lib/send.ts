191e21cff2f0e302e44a24af3746f0385463b5e0
Changlon <changlong.a2@gmail.com>
2022-01-07 18:23:44 +0800

    remove console.log

--- src/lib/send.ts
+++ src/lib/send.ts
@@ -120,7 +120,6 @@ export default class Send implements SEND.Send {
           const $data = `{"touser":"${toUser}","template_id":"${templateId}","url":"${url || ""}","miniprogram":${
             miniprogram ? JSON.stringify(miniprogram) : defaultMiniProgram ? JSON.stringify(defaultMiniProgram) : JSON.stringify({})
           },"topcolor":"${topcolor || "#ff0000"}","data":${JSON.stringify(data)}}` 
-          console.log($data)
           const token = await this.app.getAccessToken() 
           if(!token) throw new Error(`Send -- pushTemplateMsg access_token获取失败${token}`)
           const url_ = util.format(this.app.apiUrl.template,this.app.apiDomain,token)  
